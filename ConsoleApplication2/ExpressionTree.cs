be284b9c828030aeaf46bf671f75910f294f72ac
Ray Lehnhoff <smdaegan@gmail.com>
2015-10-10 17:58:24 -0600

    Added Search tree

---
+++ ConsoleApplication2/ExpressionTree.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCareerQuestionsSearch
+{
+	public class ExpressionTree
+	{
+
+	}
+
+
+	public class Node<T>
+	{
+		private T data;
+		private T initialVal;
+		private NodeList<T> neighbors = null;
+		public Node()
+		{
+
+		}
+
+		public Node(T data, T val) : this(data,null, val){}
+		public Node(T data, NodeList<T> neighbors, T val)
+		{
+			this.data = data;
+			this.neighbors = neighbors;
+			this.initialVal = val;
+		}
+
+		public T Value
+		{
+			get
+			{
+				return data;
+			}
+			set
+			{
+				data = value;
+			}
+		}
+
+		public T InitialValue
+		{
+			get
+			{
+				return initialVal;
+			}
+			set
+			{
+				initialVal = value;
+			}
+		}
+
+		protected NodeList<T> Neighbors
+		{
+			get
+			{
+				return neighbors;
+			}
+			set
+			{
+				neighbors = value;
+			}
+		}
+
+	}
+
+	public class NodeList<T> : Collection<Node<T>>
+	{
+		public NodeList() : base() { }
+
+		public NodeList(int initialSize)
+		{
+			// Add the specified number of items
+			for (int i = 0; i < initialSize; i++)
+				base.Items.Add(default(Node<T>));
+		}
+
+		public Node<T> FindByValue(T value)
+		{
+			// search the list for the value
+			foreach (Node<T> node in Items)
+				if (node.Value.Equals(value))
+					return node;
+
+			// if we reached here, we didn't find a matching node
+			return null;
+		}
+	}
+
+	public class BinaryTree<T>
+	{
+		private BinaryTreeNode<T> root;
+
+		public BinaryTree()
+		{
+			root = null;
+		}
+
+		public virtual void Clear()
+		{
+			root = null;
+		}
+
+		public BinaryTreeNode<T> Root
+		{
+			get
+			{
+				return root;
+			}
+			set
+			{
+				root = value;
+			}
+		}
+	}
+
+	public class BinaryTreeNode<T> : Node<T>
+	{
+		public BinaryTreeNode() : base() { }
+		public BinaryTreeNode(T data, T underlyingValue) : base(data, null, underlyingValue) { }
+		public BinaryTreeNode(T data, BinaryTreeNode<T> left, BinaryTreeNode<T> right)
+		{
+			base.Value = data;
+			NodeList<T> children = new NodeList<T>(2);
+			children[0] = left;
+			children[1] = right;
+			base.Neighbors = children;
+		}
+
+		public BinaryTreeNode<T> Left
+		{
+			get
+			{
+				if (base.Neighbors == null)
+					return null;
+				else
+					return (BinaryTreeNode<T>)base.Neighbors[0];
+			}
+			set
+			{
+				if (base.Neighbors == null)
+					base.Neighbors = new NodeList<T>(2);
+
+				base.Neighbors[0] = value;
+			}
+		}
+
+		public BinaryTreeNode<T> Right
+		{
+			get
+			{
+				if (base.Neighbors == null)
+					return null;
+				else
+					return (BinaryTreeNode<T>)base.Neighbors[1];
+			}
+			set
+			{
+				if (base.Neighbors == null)
+					base.Neighbors = new NodeList<T>(2);
+
+				base.Neighbors[1] = value;
+			}
+		}
+	}
+
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCareerQuestionsSearch
+{
+	class Program
+	{
+		public static Stack<string> CalcStack = new Stack<string>();
+		static void Main(string[] args)
+		{
+			//Format: Each integer is separated by a space. The value we're looking for is the last entry in the string
+			var testInput = "4 5 6 7 8 30";
+			var inputSplit = testInput.Split(' ').Select(p => int.Parse(p));
+			BinaryTree<int> tree = new BinaryTree<int>();
+			tree.Root = new BinaryTreeNode<int>(inputSplit.First(), inputSplit.First());
+			AddNodes(tree.Root, inputSplit.Skip(1).Take(inputSplit.Count() - 1));
+			var res = FindNode(tree.Root, inputSplit.Last());
+			if (res != null)
+			{
+				Console.WriteLine("Found it!");
+				while (CalcStack.Count > 0)
+				{
+					Console.Write(CalcStack.Pop());
+				}
+				Console.Write("=" + inputSplit.Last());
+			}
+			else
+			{
+				Console.WriteLine("Nope");
+			}
+			Console.Read();
+		}
+
+		public static void AddNodes(BinaryTreeNode<int> node, IEnumerable<int> nodes)
+		{
+			if (nodes.Any())
+			{
+				var multiplied = node.Value * nodes.First();
+				var added = node.Value + nodes.First();
+				
+				node.Left = new BinaryTreeNode<int>(added, nodes.FirstOrDefault());
+				node.Right = new BinaryTreeNode<int>(multiplied, nodes.FirstOrDefault());
+				nodes = nodes.Skip(1);
+				AddNodes(node.Left, nodes);
+				AddNodes(node.Right, nodes);
+			}
+		}
+
+		public static BinaryTreeNode<int> FindNode(BinaryTreeNode<int> node, int searchValue)
+		{
+			if (node != null)
+			{
+				if (node.Value == searchValue)
+				{
+					CalcStack.Push(node.InitialValue.ToString());
+					return node;
+				}
+				else
+				{
+					var right = FindNode(node.Right, searchValue);
+					if (right != null)
+					{
+						CalcStack.Push("*");
+						CalcStack.Push(node.InitialValue.ToString());
+						return right;
+					}
+					else
+					{
+						var left = FindNode(node.Left, searchValue);
+						if (left != null)
+						{
+							CalcStack.Push("+");
+							CalcStack.Push(node.InitialValue.ToString());
+							return left;
+						}
+					}
+				}
+			}
+			return null;
+		}
+	}
+}
